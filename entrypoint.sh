80bcbe298215ee142083e278968e8f0064bbc2db
gXLg <natgaev@gmail.com>
2026-01-03 16:16:28 +0100

    refactor: rely on recursive processing instead of filemask

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,9 @@ if [[ ! -d "$IDEA_DIR/bin" ]]; then
   rm /tmp/idea.tar.gz
 fi
 
+#files=$(find ./src -type f -name "*.java" | paste -sd,)
 echo "Formatting..."
-files=$(find ./src -type f -name "*.java" | paste -sd,)
-"$IDEA_DIR/bin/idea.sh" format -m "$files" -s "./style/IntelliJ_IDEA.xml" -r . #2>/dev/null
+"$IDEA_DIR/bin/idea.sh" format -s "./style/IntelliJ_IDEA.xml" -r "./src" #2>/dev/null
 echo "Finished"
 
 git --no-pager diff --exit-code
