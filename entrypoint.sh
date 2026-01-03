b6c3c9a56aa651611be063d2a6100a15bb37d376
/dev/null <natgaev@gmail.com>
2026-01-03 14:21:23 +0100

    refactor: change information printed

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,14 +22,15 @@ fi
 version=$("$IDEA_DIR/bin/idea.sh" --version 2>/dev/null || true)
 echo "Running:"
 echo "$version"
-echo "Formatting..."
 
 files=$(find ./src -type f -name "*.java")
+echo "Checking files:" $files
+
 "$IDEA_DIR/bin/format.sh" -m "$files" -s "./style/IntelliJ IDEA.xml" -r .
 
-echo "::notice title=Finished"
+echo "Finished"
 git --no-pager diff --exit-code
 if [[ "$?" != "0" ]]; then
-  echo "::warning title=Style Checks failed"
+  echo "Style Checks failed"
   exit 1
 fi
