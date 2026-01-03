60b02981852740be851f839b3951a65f34a9c9b7
/dev/null <natgaev@gmail.com>
2026-01-03 05:46:01 +0100

    fix: revert to 2025.2.6

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-  echo "Exactly one parameter required: idea-url"
-  exit 1
-fi
-
 git config --global --add safe.directory /github/workspace
 cd /github/workspace
 
-IDEA_URL=$1
 IDEA_DIR=".idea-cache"
 mkdir -p "$IDEA_DIR"
 
 if [[ ! -d "$IDEA_DIR/bin" ]]; then
   echo "Download IntelliJ IDEA..."
-  wget --no-verbose -O /tmp/idea.tar.gz "$IDEA_URL"
+  wget --no-verbose -O /tmp/idea.tar.gz "https://download.jetbrains.com/idea/ideaIC-2025.2.6.tar.gz"
   tar xzf /tmp/idea.tar.gz -C "$IDEA_DIR" --strip-components=1
   rm /tmp/idea.tar.gz
 fi
