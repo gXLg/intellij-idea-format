118ac7a5c9db9d5f48c013238f7a2efd7fb62faa
/dev/null <natgaev@gmail.com>
2026-01-03 19:47:44 +0100

    feat: improve logging and add step summary

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,13 +19,28 @@ if [[ ! -d "$IDEA_DIR/bin" ]]; then
   rm /tmp/idea.tar.gz
 fi
 
-#files=$(find ./src -type f -name "*.java" | paste -sd,)
 echo "Formatting..."
-"$IDEA_DIR/bin/idea.sh" format -s "./style/IntelliJ_IDEA.xml" -r "./src" #2>/dev/null
+"$IDEA_DIR/bin/idea.sh" format -s "./style/IntelliJ_IDEA.xml" -r "./src" 2>/dev/null
 echo "Finished"
 
-git --no-pager diff --exit-code
-if [[ "$?" != "0" ]]; then
-  echo "Style Checks failed"
+if [[ "$(git diff --shortstat)" != "" ]]; then
+  changed=yes
+  echo "# Style Checks failed" >> $GITHUB_STEP_SUMMARY
+  echo "" >> $GITHUB_STEP_SUMMARY
+  echo "Here are reformatted files:" >> $GITHUB_STEP_SUMMARY
+  echo "" >> $GITHUB_STEP_SUMMARY
+fi
+
+git diff --name-only -z | while IFS= read -r -d '' file; do
+  echo "## `$file`" >> $GITHUB_STEP_SUMMARY
+  echo "" >> $GITHUB_STEP_SUMMARY
+  echo "```diff" >> $GITHUB_STEP_SUMMARY
+  git --no-pager diff "$file" >> $GITHUB_STEP_SUMMARY
+  echo "```" >> $GITHUB_STEP_SUMMARY
+  echo "" >> $GITHUB_STEP_SUMMARY
+done
+
+if [[ "$changed" == "yes" ]]; do
+  echo "::error::Style Checks failed"
   exit 1
 fi
